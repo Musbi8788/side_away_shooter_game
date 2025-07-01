3f6804bb226439b916d19278bfe6b4b54c4da308
Musbi <musbi700@gmail.com>
2025-07-01 21:56:56 +0100

    change background colol

--- settings.py
+++ settings.py
@@ -18,5 +18,5 @@ class Settings:
         # Screen Settings
         self.screen_width = 800
         self.screen_height = 500
-        self.bg_color = (47, 79, 79)
+        self.bg_color = (0, 127, 255)
         self.game_title = "Ship Game"
