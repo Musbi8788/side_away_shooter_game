33c7c3fac85a49660f0d70b84f4f76cd0fba4f2c
Musbi <musbi700@gmail.com>
2025-07-01 23:15:12 +0100

    ship moving

--- main.py
+++ main.py
@@ -25,11 +25,43 @@ class SideWayShip():
         """
         game_is_running = True
         while game_is_running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+            self._check_events()
+            self.ship.update()
             self._update_screen()
+    
+    def _check_events(self):
+        """Respond to keypress
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypress
+        """
+        # move the ship up
+        if event.key == pygame.K_UP:
+            self.ship.moving_top = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+    
+    def _check_keyup_events(self, event):
+        """Respond to key release
+
+        Args:
+            event (_type_): _description_
+        """
+        if event.key == pygame.K_UP:
+            self.ship.moving_top = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
 
 
     def _update_screen(self):
