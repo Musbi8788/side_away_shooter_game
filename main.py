1d3fca5a1ba50dd99422b9e0c124aaa91d139753
Musbi <musbi700@gmail.com>
2025-07-01 21:55:34 +0100

    updated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
 import pygame
+from settings import Settings
 
 class SideWayShip():
     """A class manage the side away ship game
@@ -8,8 +10,37 @@ class SideWayShip():
         """Initialize the game's resources
         """
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((800, 500))
-        pygame.display.set_caption("side away shooting")
+        # using the screen setttings
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption(self.settings.game_title)
+
+        # load the game image 
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # set the position of the image in the screen
+        # self.rect.right = 
+    def run_game(self):
+        """manage the game running
+        """
+        game_is_running = True
+        while game_is_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+    def _update(self):
+        """Update the screen background and show the image 
+        """
+        self.screen.fill(self.settings.bg_color)
+        self.screen.blit(self.image, self.rect)
+
+
+if __name__ == "__main__":
+    rg = SideWayShip()
+    rg.run_game()
+
 
         
