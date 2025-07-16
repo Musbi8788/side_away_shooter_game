3407fd5b24b3c0c6599268c6acfa4a086d7df3f2
Musbi <musbi700@gmail.com>
2025-07-16 08:36:35 +0100

    ship limit equal to 3 added

--- settings.py
+++ settings.py
@@ -9,12 +9,13 @@ class Settings:
         # Bullets Settings
         self.bullet_speed = 1.5
         self.bullet_width = 15
-        self.bullet_height = 300
+        self.bullet_height = 3
         self.bullet_color = (75, 0, 130)
         self.bullets_allowed = 5
 
         # Ship Settings 
         self.ship_speed = 1.5
+        self.ship_limit = 3 # number of live the user have
 
         # Screen Settings
         self.screen_width = 850 # defualt 850
