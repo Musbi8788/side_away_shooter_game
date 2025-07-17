42053aa8f2b160f84621dcaadc2047d7e58f5334
Musbi <musbi700@gmail.com>
2025-07-17 23:04:47 +0100

    up dated

--- settings.py
+++ settings.py
@@ -21,10 +21,10 @@ class Settings:
         self.screen_width = 850 # defualt 850
         self.screen_height = 650 # 650
         self.bg_color = (130, 200, 229)
-        self.game_title = "Ship Game"
+        self.game_title = "Site away shooter"
 
         # Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right, -1 represents left.
-        self.fleet_direction = 1  # debug
+        self.fleet_direction = 1  
